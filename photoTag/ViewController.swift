dc0192fb328e62e9148d3c29f4f66e38a9504999
广智发科技 <guangzhifakeji@guangzhifakejideimac.local>
2016-07-13 17:24:59 +0800

    Initial Commit

---
+++ photoTag/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  photoTag
+//
+//  Created by stafan on 16/7/13.
+//  Copyright © 2016年 广智发科技. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
